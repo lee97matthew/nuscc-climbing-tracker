5a5e8d7f46ad2afcc8590f3061f14df69e02442f
Matthew <e0556151@u.nus.edu>
2022-07-09 16:58:30 +0800

    Fix Bot Init 8

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // create an express app
 const express = require("express");
-const app = express();
+const cors = require("cors");
+
 const axios = require("axios");
+
+require("dotenv").config();
+const app = express();
 
 // set port
 const PORT = process.env.PORT || 5000;
@@ -15,11 +19,13 @@ app.get("/", (req, res) => {
 });
 
 // open connection to NUSCCAttendanceBot
-const TOKEN = "5499274329:AAGXNvB0eL7AAoF3E6UCPT2zP_MO7v54xnM";
-const SERVER_URL = "https://nuscc-attendance-tracker.herokuapp.com";
+const { TOKEN, SERVER_URL } = process.env;
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
 const URI = `/webhook/${TOKEN}`;
 const WEBHOOK_URL = SERVER_URL + URI;
+
+console.log("web hook url is + " + WEBHOOK_URL);
+
 const { botRequest } = require("./middlewares");
 
 const botInit = async () => {
@@ -27,35 +33,36 @@ const botInit = async () => {
   const result = await axios.get(
     `${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`
   );
+  console.log(result.data);
 };
 
 app.post(URI, async (req, res) => {
   // NUSCCAttendanceBot functions
-    console.log("Enter async functions");
-    console.log(req);
-//   const chatID = req.body.message.chat.id;
-//   const teleID = req.body.message.chat.username;
+  console.log("Enter async functions");
+  console.log(req.body);
+  //   const chatID = req.body.message.chat.id;
+  //   const teleID = req.body.message.chat.username;
 
-//   if (req.body.message.text === "/update") {
-//     console.log("Update Attendance Command Match");
+  //   if (req.body.message.text === "/update") {
+  //     console.log("Update Attendance Command Match");
 
-//     // process
-//     await botRequest.teleRequest({ task: "update" });
-//   } else if (req.body.message.text === "/test") {
-//     console.log("Test Command Match");
-//     // no process
-//     await botRequest.teleRequest({ task: "test" });
-//   } else {
-//     console.log("Command Not Matched");
-//     axios.post(`${TELEGRAM_API}/sendMessage`, {
-//       chat_id: chatID,
-//       text:
-//         "Hello " +
-//         req.body.message.chat.first_name +
-//         ", Please use the /update command to update the attendance.",
-//     });
-//   }
-  return res.send();
+  //     // process
+  //     await botRequest.teleRequest({ task: "update" });
+  //   } else if (req.body.message.text === "/test") {
+  //     console.log("Test Command Match");
+  //     // no process
+  //     await botRequest.teleRequest({ task: "test" });
+  //   } else {
+  //     console.log("Command Not Matched");
+  //     axios.post(`${TELEGRAM_API}/sendMessage`, {
+  //       chat_id: chatID,
+  //       text:
+  //         "Hello " +
+  //         req.body.message.chat.first_name +
+  //         ", Please use the /update command to update the attendance.",
+  //     });
+  //   }
+    return res.send();
 });
 
 // Initialize Server
